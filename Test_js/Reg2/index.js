64660dc4241279efaf190700cc3b9c95d9c0bb43
LastW1zardGuide <96570654+LastW1zardGuide@users.noreply.github.com>
2022-03-07 00:43:29 +0600

    Add files via upload

---
+++ Test_js/Reg2/index.js
@@ -0,0 +1,22 @@
+var password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirm_password");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("error");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
+
+
+$( document ).ready(function() {
+					 $('#s-h-pass').click(function(){
+							 var type = $('#password').attr('type') == "text" ? "password" : 'text',
+									 c = $(this).html() == "<span class=\"glyphicon glyphicon-eye-close\" title=\"Скрыть пароль\"></span>" ? "<span class=\"glyphicon glyphicon-eye-open\" title=\"Показать пароль\"></span>" : "<span class=\"glyphicon glyphicon-eye-close\" title=\"Скрыть пароль\"></span>";
+							 $(this).html(c);
+							 $('#password').prop('type', type);
+					 });
+			 });
--- Test_js/Registration_form/index.js
+++ Test_js/Registration_form/index.js
@@ -11,3 +11,12 @@ function validatePassword(){
 
 password.onchange = validatePassword;
 confirm_password.onkeyup = validatePassword;
+
+$( document ).ready(function() {
+           $('#s-h-pass').click(function(){
+               var type = $('#password').attr('type') == "text" ? "password" : 'text',
+                   c = $(this).html() == "<span class=\"glyphicon glyphicon-eye-close\" title=\"Скрыть пароль\"></span>" ? "<span class=\"glyphicon glyphicon-eye-open\" title=\"Показать пароль\"></span>" : "<span class=\"glyphicon glyphicon-eye-close\" title=\"Скрыть пароль\"></span>";
+               $(this).html(c);
+               $('#password').prop('type', type);
+           });
+       });
